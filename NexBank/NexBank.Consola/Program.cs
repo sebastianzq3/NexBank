e074f131b81514323bb7042509ec1e00d9a8e81f
SEBASTIAN <sebastianzq3@gmail.com>
2026-08-25 00:20:44 -0400

    Se logró hacer la conexión de manera local a SQL Server

--- NexBank/NexBank.Consola/Program.cs
+++ NexBank/NexBank.Consola/Program.cs
@@ -1,11 +1,26 @@
-﻿namespace NexBank.Consola
+﻿using System.Data.SqlTypes;
+using Microsoft.Data.SqlClient;
+using NexBank.Logica;
+namespace NexBank.Consola
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //prueba bot discord;
+            // prueba de conexión
+            SqlConnection SqlCon = new SqlConnection();
+            SqlCon = Conexion.crearInstancia().CrearConexion();
+
+            try
+            {
+                SqlCon.Open();
+                Console.WriteLine("Conexión exitosa!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Conexión fallida");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ NexBank/NexBank.Logica/Conexion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace NexBank.Logica
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Servidor = "LAPTOP-0RU1SFNC\\SQLEXPRESS";
+            this.Base = "bd_nexbank";
+            this.Usuario = "nxb";
+            this.Clave = "nxb505";
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor +
+                                            "; Database=" + this.Base +
+                                            "; User Id=" + this.Usuario +
+                                            "; Password=" + this.Clave +
+                                            "; TrustServerCertificate=True;";
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+
+            return Cadena;
+        }
+
+        public static Conexion crearInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+
+            return Con;
+        }
+    }
+}
