28b8cda7d400ab952ad9217b7a7e0dfca8d8fbfe
SEBASTIAN <sebastianzq3@gmail.com>
2026-08-25 11:33:33 -0400

    Prototipo de interfaz de clientes y cuentas

---
+++ NexBank/NexBank.GUI/FrmRegistroCliente.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace NexBank.GUI
+{
+    partial class FrmRegistroCliente
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 57, 127);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(604, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 437);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(0, 159, 176);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(604, 100);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Cambria", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(108, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(411, 57);
+            label1.TabIndex = 0;
+            label1.Text = "Clientes y Cuentas";
+            // 
+            // FrmRegistroCliente
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(804, 437);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Font = new Font("Cambria", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FrmRegistroCliente";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+    }
+}
--- NexBank/NexBank.GUI/FrmRegistroCliente.cs
+++ NexBank/NexBank.GUI/FrmRegistroCliente.cs
@@ -1,8 +1,8 @@
 namespace NexBank.GUI
 {
-    public partial class Form1 : Form
+    public partial class FrmRegistroCliente : Form
     {
-        public Form1()
+        public FrmRegistroCliente()
         {
             InitializeComponent();
         }
